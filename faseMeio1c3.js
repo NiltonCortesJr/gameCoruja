8a9918802614b386890e0e1969c537769b9f28f9
NiltonCortesJr <niltoncjr@gmail.com>
2021-08-15 15:53:53 -0300

    Inicio do Projeto

---
+++ faseMeio1c3.js
@@ -0,0 +1,15 @@
+var escolha = ""
+while (escolha != 1 && escolha != 2) {
+  escolha = prompt("Qual é sua escolha? \n 1 - Churrascar? \n 2 - Voltar ao trabalho?")
+  if (escolha == 1) {
+    window.location.href = "./fase2c3.html"
+  } 
+
+  else if (escolha == 2) {
+    window.location.href = "./fase1c2.html"
+  }
+
+  else {
+    alert('Escolha errada')
+  } 
+}
---
+++ faseMeio2c3.js
@@ -0,0 +1,15 @@
+var escolha = ""
+while (escolha != 1 && escolha != 2) {
+  escolha = prompt("Qual é sua escolha? \n 1 - Continuar no churrasco? \n 2 - Voltar ao trabalho?")
+  if (escolha == 1) {
+    window.location.href = "./fase3c3.html"
+  } 
+
+  else if (escolha == 2) {
+    window.location.href = "./fase1c2.html"
+  }
+
+  else {
+    alert('Escolha errada')
+  } 
+}
