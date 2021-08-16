99079876585b9b49b2d70d3070359a0df6b03c1b
NiltonCortesJr <niltoncjr@gmail.com>
2021-08-15 23:28:50 -0300

    Correção de erros de português

--- faseMeio2c1.js
+++ faseMeio2c1.js
@@ -1,6 +1,6 @@
 var escolha = ""
 while (escolha != 1 && escolha != 2) {
-  escolha = prompt("Qual é sua escolha? \n 1 - Revisar código? \n 2 - Entragar o projeto?")
+  escolha = prompt("Qual é sua escolha? \n 1 - Revisar código? \n 2 - Entregar o projeto?")
   if (escolha == 1) {
     window.location.href = "./fase2c2.html"
   }
